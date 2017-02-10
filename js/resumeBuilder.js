aa3add94b5eb416469fb82d1bd3df00aa9a383eb
Jbarnum <jeryl.barnum@gmail.com>
2017-02-10 12:37:54 -0800

    altered my JS objects to follow Udacity Spec, need to comment code and tailor a bit more before submission

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,123 +2,89 @@ var bio = {
     "name": "Jeryl Barnum",
     "role": "Front-End Web Developer EXTRAORDINARE!",
     "contacts": {
-        "email": "mailto:jeryl.barnum@gmail.com?Subject=Hi There!",
         "mobile": "562-826-2655",
+        "email": "mailto:jeryl.barnum@gmail.com?Subject=Hi There!",
         "github": "https://github.com/JBarnum",
-        "linkedin": "https://www.linkedin.com/in/jbarnum",
         "twitter": "https://twitter.com/J_BARNUM",
-        "location": "Los Angeles, CA"
+        "location": "Los Angeles, CA",
+        //optional linkedin add on :)
+        "linkedin": "https://www.linkedin.com/in/jbarnum"
     },
-    "bioPic": "images/profile.jpg",
     "welcomeMessage": "What's up bro?",
-    "skills": ["Humor", "Awesomeness", "JavaScript", "CSS", "HTML5", "AllThingsGoogle", "AngularJS", "NodeJS", "jQuery"]
-};
+    "skills": ["Humor", "Awesomeness", "JavaScript", "CSS", "HTML5", "AllThingsGoogle", "AngularJS", "NodeJS", "jQuery"],
+    "bioPic": "images/profile.jpg",
+    "display": function() {
+        var name = HTMLheaderName.replace("%data%", bio.name);
+        $("#header").prepend(name);
 
-var work = {
-    "jobs": [{
-            "title": "NOC Analyst II/Systems Administrator",
-            "employer": "VectorUSA",
-            "workURL": "http://www.vectorusa.com",
-            "workLogo": "images/vector.png",
-            "dates": "March 2016-Present",
-            "location": ["Los Angeles, CA", "Oakland, CA"],
-            "description": "As a NOC Analyst I am responsible for monitoring multiple customer sites at any one time and providing general routine server routine maintenance tasks. With knowledge of TCP/IP and LAN/WAN’s I work to troubleshoot most problems effectively and with the utmost aptitude.<br>As a system administrator, I am responsible for making sure client networks and customers at client sites are up and running efficiently and effectively with as little down-time as possible. When down-time does occur, I use my abilities to effectively diagnose the problem and come up with a solution ASAP."
-        },
-        {
-            "title": "Network Support Specialst",
-            "employer": "Farmers & Merchants Bank",
-            "workURL": "http://www.fmb.com",
-            "workLogo": "images/fmb.png",
-            "dates": "March 2009-Febuary 2015",
-            "location": "Long Beach, CA",
-            "description": "As a Network Services Support Associate, I am responsible for supporting the day-to-day operations of the Bank's Information Systems by resolving how-to questions, performing security resets and basic troubleshooting of system problems. Additionally, I set up, install and maintain printers, workstations, notebooks, and servers at the bank locations."
+        var formattedContact = HTMLemail.replace("%data%", bio.contacts.email);
+        var formattedPhone = HTMLmobile.replace("%data%", bio.contacts.mobile);
+        var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+        var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
+        var formattedGit = HTMLgithub.replace("%data%", bio.contacts.github);
+        var formattedLinkedin = HTMLlinkedin.replace("%data%", bio.contacts.linkeadin);
+        var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+
+        $("#header").prepend(formattedPic);
+        $("#topContacts").prepend(formattedContact,
+            formattedPhone,
+            formattedTwitter,
+            formattedGit,
+            formattedLinkedin,
+            formattedLocation
+        );
+
+        var formattedSkill;
+
+        //Loop through bio.skills object and display it instead of repeating your code
+        $("#header").append(HTMLskillsStart);
+        for (var i = 0; i < bio.skills.length; i++) {
+
+            formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+
+            $("#skills").append(formattedSkill);
         }
-    ]
+    }
 };
 
 var education = {
     "schools": [{
             "name": "CSULB",
-            "city": "Long Beach, CA",
+            "location": "Long Beach, CA",
             "degree": "Bachelor of Science",
-            "major": "Management Information Systems & Finance/Investments",
-            "dates": "2005-2010"
+            "majors": "Management Information Systems & Finance/Investments",
+            "dates": "2005-2010",
+            "url": "http://www.csulb.edu"
         },
         {
             "name": "Polytechnic HS",
-            "city": "Long Beach, CA",
+            "location": "Long Beach, CA",
             "degree": "You Don't Major in High School",
-            "major": "AP Classes",
-            "dates": "Graduation -- 2005"
+            "majors": "AP Classes",
+            "dates": "Graduation -- 2005",
+            "url": "http://lbpoly.schoolloop.com/"
         }
     ],
     "onlineCourses": [{
-            "name": "Udacity",
-            "course": "Front-End Development"
+            "title": "Front-End Development",
+            "school": "Udacity",
+            "dates": "Nov 2016-Present",
+            "url": "https://www.udacity.com/"
         },
         {
-            "name": "CodeAcademy",
-            "course": "JavaScript"
+            "title": "JavaScript",
+            "school": "CodeAcademy",
+            "dates": "Off and On",
+            "url": "https://www.codecademy.com/"
         },
         {
-            "name": "FreeCodeCamp",
-            "course": "Web Development"
+            "title": "Web Development",
+            "school": "FreeCodeCamp",
+            "dates": "Here and there",
+            "url": "https://www.freecodecamp.com/"
         }
-    ]
-};
-
-var projects = {
-    "projects": [{
-            "title": "XO Crew",
-            "dates": "January 2014",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "images": "images/tic.png"
-        },
-        {
-            "title": "Grub1Out",
-            "dates": "Febuary 2015",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "images": "images/Grub_One_Out.png"
-        },
-        {
-            "title": "TrailMix",
-            "dates": "April 2015",
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "images": "images/trailmix.png"
-        }
-    ]
-};
-
-
-displaySite();
-
-//function that displays site
-function displaySite() {
-
-    //create a projects display property
-    projects.display = function() {
-
-        for (var i = 0; i < projects.projects.length; i++) {
-            console.log(projects.projects[i].title);
-            $("#projects").append(HTMLprojectStart);
-
-            var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
-            $(".project-entry:last").append(formattedTitle);
-
-            var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
-            $(".project-entry:last").append(formattedDates);
-
-            var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
-            $(".project-entry:last").append(formattedDescription);
-
-            var formattedImage;
-            formattedImage = HTMLprojectImage.replace("%data%", projects.projects[i].images);
-            $(".project-entry:last").append(formattedImage);
-        }
-    };
-
-    //start education display property function here
-    education.display = function() {
+    ],
+    "display": function() {
 
         var formattedSchoolName;
         var formattedSchoolDegree;
@@ -127,9 +93,9 @@ function displaySite() {
 
         for (var i = 0; i < education.schools.length; i++) {
             $("#education").append(HTMLschoolStart);
-            formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].name);
+            formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].location);
             formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
-            formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].major);
+            formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].majors);
 
             var formattedSchoolEntry = formattedSchoolName + formattedSchoolDegree + formattedSchoolMajor;
             $(".education-entry:last").append(formattedSchoolEntry);
@@ -158,44 +124,30 @@ function displaySite() {
         //     $(".education-entry").append(formattedOnlineTitle);
         // });
 
-    };
+    }
+};
 
-    //bio display property
-    bio.display = function() {
-        var name = HTMLheaderName.replace("%data%", bio.name);
-        $("#header").prepend(name);
-
-        var formattedContact = HTMLemail.replace("%data%", bio.contacts.email);
-        var formattedPhone = HTMLmobile.replace("%data%", bio.contacts.mobile);
-        var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-        var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
-        var formattedGit = HTMLgithub.replace("%data%", bio.contacts.github);
-        var formattedLinkedin = HTMLlinkedin.replace("%data%", bio.contacts.linkedin);
-        var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-
-        $("#header").prepend(formattedPic);
-        $("#topContacts").prepend(formattedContact,
-            formattedPhone,
-            formattedTwitter,
-            formattedGit,
-            formattedLinkedin,
-            formattedLocation
-        );
-
-        var formattedSkill;
-
-        //Loop through bio.skills object and display it instead of repeating your code
-        $("#header").append(HTMLskillsStart);
-        for (var i = 0; i < bio.skills.length; i++) {
-
-            formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
-
-            $("#skills").append(formattedSkill);
+var work = {
+    "jobs": [{
+            "employer": "VectorUSA",
+            "title": "NOC Analyst II/Systems Administrator",
+            "location": "Los Angeles, CA",
+            "dates": "March 2016-Present",
+            "description": "As a NOC Analyst I am responsible for monitoring multiple customer sites at any one time and providing general routine server routine maintenance tasks. With knowledge of TCP/IP and LAN/WAN’s I work to troubleshoot most problems effectively and with the utmost aptitude.<br>As a system administrator, I am responsible for making sure client networks and customers at client sites are up and running efficiently and effectively with as little down-time as possible. When down-time does occur, I use my abilities to effectively diagnose the problem and come up with a solution ASAP.",
+            "workURL": "http://www.vectorusa.com",
+            "workLogo": "images/vector.png"
+        },
+        {
+            "employer": "Farmers & Merchants Bank",
+            "title": "Network Support Specialst",
+            "location": "Long Beach, CA",
+            "dates": "March 2009-Febuary 2015",
+            "description": "As a Network Services Support Associate, I am responsible for supporting the day-to-day operations of the Bank's Information Systems by resolving how-to questions, performing security resets and basic troubleshooting of system problems. Additionally, I set up, install and maintain printers, workstations, notebooks, and servers at the bank locations.",
+            "workURL": "http://www.fmb.com",
+            "workLogo": "images/fmb.png"
         }
-    };
-
-    //work display property
-    work.display = function() {
+    ],
+    "display": function() {
         //Display Work loop
         for (var i = 0; i < work.jobs.length; i++) {
             $("#workExperience").append(HTMLworkStart);
@@ -214,10 +166,55 @@ function displaySite() {
 
             var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
             $(".work-entry:last").append(formattedDescription);
-
         }
-    };
+    }
+};
 
+var projects = {
+    "projects": [{
+            "title": "XO Crew",
+            "dates": "January 2014",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "images": ["images/tic.png"]
+        },
+        {
+            "title": "Grub1Out",
+            "dates": "Febuary 2015",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "images": ["images/Grub_One_Out.png"]
+        },
+        {
+            "title": "TrailMix",
+            "dates": "April 2015",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "images": ["images/trailmix.png"]
+        }
+    ],
+    "display": function() {
+        for (var i = 0; i < projects.projects.length; i++) {
+            $("#projects").append(HTMLprojectStart);
+
+            var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
+            $(".project-entry:last").append(formattedTitle);
+
+            var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
+            $(".project-entry:last").append(formattedDates);
+
+            var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
+            $(".project-entry:last").append(formattedDescription);
+
+            var formattedImage;
+            formattedImage = HTMLprojectImage.replace("%data%", projects.projects[i].images);
+            $(".project-entry:last").append(formattedImage);
+        }
+    }
+};
+
+
+displaySite();
+
+//function that displays site
+function displaySite() {
     projects.display();
     bio.display();
     work.display();
